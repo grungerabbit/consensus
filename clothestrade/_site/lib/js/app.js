c384b146f56a59a42dcfcaccb2daa0cfdda951fa
Cheryl Wu <grungerabbit@gmail.com>
2014-07-17 17:27:38 -0400

    basic setup of inventory itemize

--- clothestrade/_site/lib/js/app.js
+++ clothestrade/_site/lib/js/app.js
@@ -1,5 +1,82 @@
 function CT($scope) {
-	$scope.test = "AA"
+	$scope.inventory = [
+		{
+			"type": "Shirts/tops",
+			"count": 0
+		},
+		{
+			"type": "Button downs",
+			"count": 0
+		},
+		{
+			"type": "Sweaters",
+			"count": 0
+		},
+		{
+			"type": "Vests",
+			"count": 0
+		},
+		{
+			"type": "Jackets",
+			"count": 0
+		},
+		{
+			"type": "Pants",
+			"count": 0
+		},
+		{
+			"type": "Skirts",
+			"count": 0
+		},
+		{
+			"type": "Suits",
+			"count": 0
+		},
+		{
+			"type": "Dresses",
+			"count": 0
+		},
+		{
+			"type": "Shoes",
+			"count": 0
+		},
+		{
+			"type": "Hats",
+			"count": 0
+		},
+		{
+			"type": "Bags",
+			"count": 0
+		},
+		{
+			"type": "Belts",
+			"count": 0
+		},
+		{
+			"type": "Gloves",
+			"count": 0
+		},
+		{
+			"type": "Scarves",
+			"count": 0
+		},
+		{
+			"type": "Misc",
+			"count": 0
+		},
+	];
+	
+	$scope.decreaseCount = function(item) {
+		console.log(item)
+	}
+	
+	$scope.increaseCount = function(item) {
+		console.log(item)
+	}
+	
+	
+	
+	
 }
 
 
