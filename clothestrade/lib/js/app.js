32990b3b08a64efbd978975bf4db1cf86d6c13bd
Cheryl Wu <grungerabbit@gmail.com>
2014-07-17 21:05:05 -0400

    misc toggle, mailto itemizer

--- clothestrade/lib/js/app.js
+++ clothestrade/lib/js/app.js
@@ -1,71 +1,82 @@
 angular.module('CT', ['ngTouch']).controller("calc", calc);
 
-function calc($scope) {
+function calc($scope, $window) {
 	$scope.inventory = [
 		{
 			"type": "Shirts/tops",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Button downs",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Sweaters",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Vests",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Jackets",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Pants",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Skirts",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Suits",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Dresses",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Shoes",
-			"count": 0
+			"count": 0,
+			"misc": false
 		},
 		{
 			"type": "Hats",
-			"count": 0
+			"count": 0,
+			"misc": true
 		},
 		{
 			"type": "Bags",
-			"count": 0
+			"count": 0,
+			"misc": true
 		},
 		{
 			"type": "Belts",
-			"count": 0
+			"count": 0,
+			"misc": true
 		},
 		{
 			"type": "Gloves",
-			"count": 0
+			"count": 0,
+			"misc": true
 		},
 		{
 			"type": "Scarves",
-			"count": 0
-		},
-		{
-			"type": "Misc",
-			"count": 0
-		},
+			"count": 0,
+			"misc": true
+		}
 	];
 	
 	$scope.decreaseCount = function(item) {
@@ -75,6 +86,8 @@ function calc($scope) {
 	$scope.increaseCount = function(item) {
 		$scope.inventory[item].count++;
 	}
+	
+	$scope.misc = false;
 	
 	$scope.totalItems = function() {
 		var total = 0;
@@ -88,10 +101,45 @@ function calc($scope) {
 	var Item = function() {
 		this.type = "New Category";
 		this.count = 0;
+		this.misc = true;
 	}
 	
 	$scope.addCategory = function() {
 		$scope.inventory.push(new Item());
+	}
+	
+	$scope.toggleMisc = function() {
+		$scope.misc = !$scope.misc;
+	}
+	
+	$scope.generateItemBreakdown = function() {
+		var items = $scope.inventory;
+		
+		var email = [];
+		
+		function assembleMailto(item) {
+			var type = item.type;
+			var count = item.count;
+			
+			var line = type + ": " + count;
+			
+			stripSpaces(line);
+		}
+		
+		function stripSpaces(line) {
+			var newLine = line.replace(/\s/g, "%20") + "%0D%0A";
+			email.push(newLine);
+		}
+		
+		for (var k = 0; k < items.length; k++) {
+			assembleMailto(items[k])
+		}
+		
+		email.push(stripSpaces("Total: " + $scope.totalItems()));
+		
+		console.log("mailto:test@gmail.com?subject=Clothestrade%202014&body=" + email.join(""))
+		
+		$window.location = "mailto:test@gmail.com?subject=Clothestrade%202014&body=" + email.join("");
 	}
 }
 
